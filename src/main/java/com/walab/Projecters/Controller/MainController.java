aef53ba9be2fdda469d926e0b4efbb049a177d4e
chamroro <22000197@handong.edu>
2022-05-13 16:43:29 +0900

    add project controller

--- src/main/java/com/walab/Projecters/Controller/MainController.java
+++ src/main/java/com/walab/Projecters/Controller/MainController.java
@@ -15,4 +15,9 @@ public class MainController {
 	public String main() {		
 		return "Main";
 	}
+	
+	@RequestMapping(value = "/project", method = RequestMethod.GET)
+	public String project() {		
+		return "Project";
+	}
 }
